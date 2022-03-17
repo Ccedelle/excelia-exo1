a682174d6358cc5905f4e0ee6c550a40ed0748b5
CEDELLE Charles <charles.cedelle@businessdecision.com>
2022-03-17 10:29:51 +0100

    Initial commit

---
+++ migration.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+print("Creating DB")
+conn = sqlite3.connect("fb.db")
+print("DB created")
+
+conn.execute('CREATE TABLE IF NOT EXISTS facebook ( id INTEGER PRIMARY KEY AUTOINCREMENT, username char(100) NOT NULL UNIQUE, email char(100) NOT NULL UNIQUE, password char(100) NOT NULL, cookie char(128) UNIQUE)')
+conn.execute("INSERT INTO facebook (username, email, password) VALUES ('Charles', 'charlescedelle@orange.fr', 'test')")
+conn.commit()
+print("Table create")
